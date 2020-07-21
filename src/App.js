ec5ddc91ede6865ce6b258137002f0e4a7973626
Wojciech Knott <wojciechknott@gmail.com>
2020-07-21 23:06:00 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Container from "./Container";
+import Header from "./Header";
 
 function App() {
   return (
     <Container>
+      <Header />
     </Container>
   );
 }
---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => (
+  <header class="header">
+    <h1 class="header__title">Przelicznik walut</h1>
+  </header>
+)
+
+export default Header;
