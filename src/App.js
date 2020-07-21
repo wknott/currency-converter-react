4ceadece5118ab32b616ed6586b924cfd9852959
Wojciech Knott <wojciechknott@gmail.com>
2020-07-21 23:03:28 +0200

    added contanier component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
+import Container from "./Container";
 
 function App() {
   return (
-    <div></div>
+    <Container>
+    </Container>
   );
 }
 
