ca610e87de7e53cd966ac6bbf322aaaffcde09e2
Wojciech Knott <wojciechknott@gmail.com>
2020-07-22 19:25:06 +0200

    added form field component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,40 +1,33 @@
 import React from "react";
 import "./styles.css";
+import FormField from "../FormField";
 
 const Form = ({ amount, setAmount }) => (
   <form class="form">
     <fieldset class="form__fieldset">
       <legend class="form__legend">Przelicznik</legend>
-      <p>
-        <label class="form__label">
-          <span class="form__labelText">Pierwsza waluta:</span>
-          <select class="form__select">
-            <option>PLN</option>
-            <option>EUR</option>
-            <option>USD</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label class="form__label">
-          <span class="form__labelText">Kwota:</span>
-          <input
-            class="form__field js-amount"
-            type="number" value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-          />
-        </label>
-      </p>
-      <p>
-        <label class="form__label">
-          <span class="form__labelText">Druga waluta:</span>
-          <select class="form__select js-selectTo">
-            <option>PLN</option>
-            <option>EUR</option>
-            <option>USD</option>
-          </select>
-        </label>
-      </p>
+      <FormField labelText="Pierwsza waluta">
+        <select class="form__select">
+          <option>PLN</option>
+          <option>EUR</option>
+          <option>USD</option>
+        </select>
+      </FormField>
+      <FormField labelText="Kwota">
+        <input
+          class="form__field"
+          type="number"
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
+        />
+      </FormField>
+      <FormField labelText="Druga waluta">
+        <select class="form__select js-selectTo">
+          <option>PLN</option>
+          <option>EUR</option>
+          <option>USD</option>
+        </select>
+      </FormField>
     </fieldset>
   </form>
 )
